95c773ff0b058e0aefefde3e5717bcaee88e8eff
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-22 15:39:36 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ Bundler.require
 module Concerns
   module Findable
     def self.find_by_name(name)
-      self.all.find {|item| item.name == name}
+      all.find {|item| item.name == name}
     end
   end
 end
