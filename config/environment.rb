39c9ce0d08924c761d1f61bbb7a67a4b98cf561b
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-22 15:41:46 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,28 @@ Bundler.require
 
 module Concerns
   module Findable
+
     def find_by_name(name)
       self.all.find {|item| item.name == name}
     end
+
+    def find_or_create_by_name(name)
+      if find_by_name(name) == nil
+        self.new(name)
+      else
+        find_by_name(name)
+      end
+    end
+
+
+      # def self.find_or_create_by_name(name)
+      #   if find_by_name(name) == nil
+      #     Song.create(name)
+      #   else
+      #     find_by_name(name)
+      #   end
+      # end
+    
   end
 end
 
