32a9ad61937221994ac3ba7d58204695eb9cdc8a
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-22 15:17:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -47,7 +47,7 @@ grunge = Genre.new("Genre A")
 rock = Genre.new("Genre B")
 c = Song.new("SONG 1", a, grunge)
 d = Song.new("SONG 2", a, rock)
-puts c.artist.name
+puts c.genre
 
 # expect(artist.genres).to include(genre)
 # expect(artist.genres).to include(other_genre)
